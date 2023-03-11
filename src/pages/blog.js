d16865136795dae0be031d115a6586598e107a2e
KKY <kikisiub11@gmail.com>
2023-03-11 18:07:32 +0700

    [pages]

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,13 @@
 /* eslint-disable prettier/prettier */
 import React from "react";
 import BlogItemContainer from "../containers/blog/blog-item";
-import NewsletterArea from "../containers/global/newsletter";
+//import NewsletterArea from "../containers/global/newsletter";
 import PageBanner from "../containers/global/page-banner";
 import FooterCopyright from "../layouts/footer_copyright";
 import Header from "../layouts/header";
 import Layout from "../layouts/index";
 import BlogData from "../data/blog.json";
-import ScrollToTop from "../components/scroll-to-top";
+// import ScrollToTop from "../components/scroll-to-top";
 import SEO from "../components/seo";
 
 const BlogPage = () => {
